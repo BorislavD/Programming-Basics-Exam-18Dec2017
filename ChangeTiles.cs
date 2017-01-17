aa0afe4cf3fc417d0aaa51130c4653da1f4b7ea2
Borislav Dermendzhiev <lenamarltd@gmail.com>
2017-01-17 17:17:53 +0200

    Added Change Tiles Added Distance

---
+++ ChangeTiles.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeTiles
+{
+    class ChangeTiles
+    {
+        static void Main(string[] args)
+        {
+            decimal money = decimal.Parse(Console.ReadLine());
+            decimal floorWidth = decimal.Parse(Console.ReadLine());
+            decimal floorLength = decimal.Parse(Console.ReadLine());
+            decimal triangleSide = decimal.Parse(Console.ReadLine());
+            decimal triangleHeight = decimal.Parse(Console.ReadLine());
+            decimal plochkaPrice = decimal.Parse(Console.ReadLine());
+            decimal sumaMaistor = decimal.Parse(Console.ReadLine());
+
+            decimal floorArea = floorWidth * floorLength;
+            decimal plochkaArea = triangleSide * triangleHeight / 2;
+            decimal plochkaRequired = Math.Ceiling(floorArea / plochkaArea + 5);
+            decimal sumaPlochki = plochkaPrice * plochkaRequired + sumaMaistor;
+
+            if (money >= sumaPlochki)
+            {
+                Console.WriteLine("{0:f2} lv left.", money - sumaPlochki);
+
+            }
+            else
+            {
+                Console.WriteLine("You'll need {0:f2} lv more.", sumaPlochki - money);
+            }
+        }
+    }
+}
---
+++ Distance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distance
+{
+    class Distance
+    {
+        static void Main(string[] args)
+        {
+            int speedFirst = int.Parse(Console.ReadLine());
+            int timeFirstMinutes = int.Parse(Console.ReadLine());
+            int timeSecondMinutes = int.Parse(Console.ReadLine());
+            int timeThirdMinutes = int.Parse(Console.ReadLine());
+
+            double timeFirstHours = timeFirstMinutes / 60.00;
+            double timeSecondHours = timeSecondMinutes / 60.00;
+            double timeThirdHours = timeThirdMinutes / 60.00;
+
+            double speedSecond = speedFirst * 1.10;
+            double speedThird = speedSecond * 0.95;
+
+            double raztojanieFirst = speedFirst * timeFirstHours;
+            double raztojanieSecond = speedSecond * timeSecondHours;
+            double raztojanieThird = speedThird * timeThirdHours;
+
+            double raztojanieTotal = raztojanieFirst + raztojanieSecond + raztojanieThird;
+
+            Console.WriteLine("{0:f2}", raztojanieTotal);
+        }
+    }
+}
